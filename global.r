1664b0e0ba15774952716a041bf543f4afd2e253
ShengweiHuang <huang6275@gmail.com>
2017-07-13 21:28:04 +0800

    global.r

--- global.r
+++ global.r
@@ -1,11 +1,8 @@
 library(shiny)
 library(plotly)
-library(XML)
 library(RCurl)
 
 currencyType = c("USD", "CAD", "JPY", "HKD", "GBP", "AUD", "SGD", "CHF")
-type1 = c("1 Day", "3 Month", "6 Month")
-yn = c("No", "Yes")
 buySell = c("Buy", "Sell")
 cashSpot = c("Cash", "Spot")
 cName = c("time","貨幣別","買進量","買進價","賣出量","賣出價","交易形式","總價格","出售匯率","獲利")
