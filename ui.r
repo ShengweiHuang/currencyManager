c078b8bc7e70086245ee7f1fd631083bef4e02b4
ShengweiHuang <huang6275@gmail.com>
2017-07-13 21:28:21 +0800

    ui.r

--- ui.r
+++ ui.r
@@ -2,31 +2,20 @@ shinyUI(fluidPage(
   titlePanel("personal currency trade record"),
   sidebarLayout(
     sidebarPanel(
-      ## personal record
-      helpText("Find Personal Record"),
-      textInput("id", "input your id", value=""), # input record id
-      ## ask add record
-      selectInput("addRecord", "Add a Record?", # input type and tag
-                  choices = yn), # default value
-      ## end
       ## input record
-      selectInput("recordCurrencyType", "Choose a currency type:", # input type and tag
-                  choices = currencyType), # default value
-      selectInput("buySellSelect", "Buy or sell:", # input type and tag
-                  choices = buySell), # default value
-      selectInput("cashSpotSelect", "Cash or spot:", # input type and tag
-                  choices = cashSpot), # default value
-      textInput("quantity", "Quantity", value=""), # input quantity
-      textInput("price", "Price", value=""), # input quantity
+      selectInput("recordCurrencyType", "Choose a currency type:",
+                  choices = currencyType),
+      selectInput("buySellSelect", "Buy or sell:",
+                  choices = buySell),
+      selectInput("cashSpotSelect", "Cash or spot:",
+                  choices = cashSpot),
+      textInput("quantity", "Quantity", value=""),
+      textInput("price", "Price", value=""),
       ## end
       submitButton("submit") # submit data
-      #label = "Submit", inputId = "submit"
     ),
     mainPanel(
-      conditionalPanel(
-        condition = "input.id != ''",
-        tableOutput("recordTable")
-      )
+      tableOutput("recordTable")
     )
   )
 ))
